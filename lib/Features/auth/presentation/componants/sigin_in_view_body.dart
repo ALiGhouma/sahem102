36c983a90383970fdd20586ddfdc1994e0e86e45
aimen94 <aimenelwhab@gmail.com>
2024-01-27 00:56:04 +0200

    authuiUpdate

---
+++ lib/Features/auth/presentation/componants/sigin_in_view_body.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sahem/Core/resources/app_strings.dart';
+import 'package:sahem/Core/resources/color_manager.dart';
+import 'package:sahem/Core/resources/font_manger.dart';
+import 'package:sahem/Core/resources/style_manager.dart';
+import 'package:sahem/Core/utils/space_adder.dart';
+import 'package:sahem/Features/auth/presentation/componants/sigin_in_form.dart';
+
+class SiginInViewBody extends StatelessWidget {
+  const SiginInViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: ColorManager.primary,
+        title: Text(
+          AppStrings.loginTitle,
+          style:
+              getBoldStyle(color: ColorManager.white, fontSize: FontSize.s14),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            addVerticalSpace(15),
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                height: 270.h,
+                width: 270.w,
+                decoration: BoxDecoration(
+                    //color: ColorManager.primary,
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          ColorManager.primary,
+                          Color.fromARGB(255, 17, 150, 116)
+                        ])),
+                child: Center(
+                  child: Text(
+                    AppStrings.loginbody,
+                    style: getRegularStyle(
+                        color: ColorManager.white, fontSize: FontSize.s18),
+                  ),
+                ),
+              ),
+            ),
+            addVerticalSpace(20),
+            SiginInForm(),
+            addVerticalSpace(32.h),
+            TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  minimumSize: Size(MediaQuery.of(context).size.width.w, 50),
+                  backgroundColor: ColorManager.primary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10.0), // Adjust the radius as needed
+                  ),
+                ),
+                child: Text(
+                  AppStrings.login,
+                  style: getRegularStyle(color: ColorManager.white),
+                )),
+            addVerticalSpace(16.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  AppStrings.dontHaveAccount,
+                  style: getRegularStyle(fontSize: FontSize.s12),
+                ),
+                // addHorizontalSpace(1.w),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      AppStrings.register,
+                      style: getRegularStyle(
+                          color: ColorManager.primary, fontSize: FontSize.s14),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Features/auth/presentation/sigin_in_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:sahem/Features/auth/presentation/componants/sigin_in_view_body.dart';
+
+class SiginInView extends StatelessWidget {
+  const SiginInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SiginInViewBody();
+  }
+}
